12d8e89c91376334c5356118c4cebbdd34d0a022
Mrwan <mrwan8khaled@gmail.com>
2025-07-20 14:44:28 +0300

    fix style & make settings menu

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-let pageStyle = localStorage.getItem("pageStyle") || "styles/style.css";
-// document.getElementById("pageStyle").setAttribute("href", pageStyle);
+let pageStyle = localStorage.getItem("pageStyle") || "xp.css";
+document.getElementById("pageStyle").setAttribute("href", "styles/"+pageStyle);
+
+let styleSelect = document.getElementById("styleSelect");
+styleSelect.addEventListener("change", function() {
+  let selectedStyle = styleSelect.value;
+    localStorage.setItem("pageStyle", selectedStyle);
+    document.getElementById("pageStyle").setAttribute("href", "styles/"+selectedStyle);
+    window.location.reload();
+});
+
+let settingsButton = document.getElementById("settings");
+let settingsMenu = document.querySelector(".settingsMenu");
+
+settingsButton.addEventListener("click", function() {
+  if (settingsMenu.style.display === "none" || settingsMenu.style.display === "") {
+    settingsMenu.style.display = "block";
+  } else {
+    settingsMenu.style.display = "none";
+  }
+})
